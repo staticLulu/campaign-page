0c18ad1d366750c88528d9fb19398be66e6e70fe
dangmakara <dang.dangmakara@gmail.com>
2024-02-06 14:21:23 +0700

    init

---
+++ src/app/home/page.tsx
@@ -0,0 +1,12 @@
+import HomeSection from '@/components/HomeSection';
+
+const HomeSectionPage = () => {
+  return (
+    <div>
+      <p>home section page</p>
+      <HomeSection />
+    </div>
+  );
+};
+
+export default HomeSectionPage;
---
+++ src/components/HomeSection/index.tsx
@@ -0,0 +1,7 @@
+'use client';
+
+const HomeSection = () => {
+  return <p>home section</p>;
+};
+
+export default HomeSection;
